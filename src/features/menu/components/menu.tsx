a6e2925da7ddd908cacbb2eba56334863c45728b
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-17 19:12:49 +0200

    feat: create basic routes which is needed to go in website

--- src/features/menu/components/menu.tsx
+++ src/features/menu/components/menu.tsx
@@ -5,14 +5,13 @@ export const Menu = () => {
     return (
         <div>
             <nav className='text-purple-800 flex justify-center h-[100px]'>
-                <ul className='flex justify-between w-[1000px] items-center'>
+                <ul className='flex justify-between w-[700px] items-center'>
                     <li>
                         <h1 className='text-3xl'>stewardship</h1>
                     </li>
                     <NavLinkComponent path="/" name="Home"/>
-                    <NavLinkComponent path="/about" name="About"/>
-                    <NavLinkComponent path="/contact" name="Contact"/>
                     <NavLinkComponent path="/login" name="Login"/>
+                    <NavLinkComponent path="/register" name="Register"/>
                 </ul>
             </nav>
         </div>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,13 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 
 import HomePage from "../pages/home/index";
 import RootPage from "../pages/root/index";
+import LoginPage from "../pages/login/index";
+import RegisterPage from "../pages/register/index";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
         element: <RootPage />,
         children: [
-            {path: "/", element: <HomePage />}
+            {path: "/", element: <HomePage />},
+            {path: "/login", element: <LoginPage />},
+            {path: "/register", element: <RegisterPage />}
         ]
     }
 ])
