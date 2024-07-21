cd9322e23a45d188253a95a386bc5738e240d752
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-21 17:21:47 +0200

    feat: create loading component

---
+++ src/shared/components/loading.tsx
@@ -0,0 +1,15 @@
+import { Triangle } from "react-loader-spinner";
+
+const Loading: React.FC<{size: number}> = ({size}) => {
+    return (
+        <Triangle
+            visible={true}
+            height={size}
+            width={size}
+            color="#7e007e"
+            ariaLabel="triangle-loading"
+        />
+    )
+}
+
+export default Loading;
