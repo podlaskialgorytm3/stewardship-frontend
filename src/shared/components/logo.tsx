371de3c3c6965a4879e3841b9b32b5b6969cf478
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-24 21:56:52 +0200

    refactor: change func of logo | add link to logo

--- src/shared/components/logo.tsx
+++ src/shared/components/logo.tsx
@@ -1,9 +1,12 @@
 import LogoImg from '../../assets/logo.webp';
+import { NavLink } from 'react-router-dom';
 
 export const Logo: React.FC<{isImage: boolean}> = ({isImage}) => {
     return(
-            <h1 className='text-3xl flex justify-center items-center'>
+            <NavLink to='/'>
+                <h1 className='text-3xl flex justify-center items-center'>
                 {isImage ? <img src={LogoImg} alt='logo' className='w-[50px] mr-5'/> : ''}
                 stewardship</h1>
+            </NavLink>
     )
 }
