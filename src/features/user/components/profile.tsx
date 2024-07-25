2d5f8fdb0583736dca737b2510c6ddca38abc81e
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-25 14:45:13 +0200

    feat: improvment the appearance of profile photo

--- src/features/user/components/profile.tsx
+++ src/features/user/components/profile.tsx
@@ -7,19 +7,20 @@ export const Profile: React.FC = () => {
     const { data, isLoading } = useFetchUser();
     const { handleImageClick, handleNameClick, handleEmailClick } = useHandleEdit({data});
 
-
     return(
         <div className="min-h-[50vh] flex justify-center items-center flex-col">
             {isLoading && <Loading  size={100}/>}
             <div className="flex justify-center items-center flex-col">
                 <h1 className="text-3xl font-bold">Profile</h1>
                 <div className="flex flex-col items-center">
-                    <img 
-                        src={data?.img} 
-                        alt={data?.name} 
-                        className="w-[150px] h-[150px] rounded-[50%] mt-10 bg-center" 
-                        onClick={() => handleImageClick()}
+                {(data || !isLoading) && 
+                <img 
+                    src={data.img ? data.img : ""} 
+                    alt="profile" 
+                    className="w-[200px] h-[200px] rounded-full object-cover border-[#7e007e] border-[3px] mt-10"
+                    onClick={() => handleImageClick()}
                     />
+                }
                     <p 
                         className="text-3xl mt-10 font-bold"
                         onClick={() => handleNameClick()}
