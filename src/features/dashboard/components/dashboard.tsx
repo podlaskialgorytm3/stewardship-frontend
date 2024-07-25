7b20b8eb762a2eb4373c9f70b668d3da3cd0a7e2
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-25 22:38:47 +0200

    feat: create special view

--- src/features/dashboard/components/dashboard.tsx
+++ src/features/dashboard/components/dashboard.tsx
@@ -1,13 +1,14 @@
 import { MenuDashboard } from "./menu-dashboard";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export const Dashboard: React.FC = () => {
+    const location = useLocation();
     return (
         <div className="flex items-center flex-col">
             <h1 className="text-3xl font-bold">Dashboard</h1>
             <MenuDashboard />
             <Outlet />
+            {location.pathname === "/dashboard" && <h1 className="text-3xl font-bold">Welcome to your dashboard</h1>}
         </div>
     );
 }
-
