c61ed2b1c57afb16033af5b9c40a2cd594e6c37f
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-26 15:57:25 +0200

    feat: create custom hook to getting groups by name

---
+++ src/features/groups/api/use-search-group.ts
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+import Swal from "sweetalert2";
+
+const searchGroup = async (name: string) => {
+    const response = await fetch(`${API_URL}/stewardship/group/${name}`,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+
+    if(!response.ok){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+        });
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+const useSearchGroup = (name: string) => (
+    useQuery({
+        queryKey: ['group', name],
+        queryFn: () => searchGroup(name)
+    })
+)
+
+export default useSearchGroup;
