e81653b7e194dfc1437c1f880d6d6b9742737580
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-28 17:00:31 +0200

    feat: create file with types of member

---
+++ src/features/groups/constants/constants.ts
@@ -0,0 +1,17 @@
+export const MEMBERSHIPMENT = [
+    {
+        type: "member",
+        color: "bg-[#7e007e]",
+        text: "View Group"
+    },
+    {
+        type: "pending",
+        color: "bg-[#ff9900]",
+        text: "Pending"
+    },
+    {
+        type: "none",
+        color: "bg-[#00d941]",
+        text: "Join Group"
+    }
+]
