732afb00404d4ca47a27cac4b72bc4264dc606df
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-28 18:03:58 +0200

    feat: write custom hook to send call to the API

--- src/features/groups/components/group-button.tsx
+++ src/features/groups/components/group-button.tsx
@@ -1,13 +1,25 @@
 import { MEMBERSHIPMENT } from "../constants/constants"
 
-export const GroupButton = ({typeOfMember} : {typeOfMember: string}) => {
+export const GroupButton = ({typeOfMember,groupId} : {typeOfMember: string, groupId: number}) => {
     const membership: {
         type: string;
         color: string;
         text: string;
     } | any = MEMBERSHIPMENT.find((membership) => membership.type === typeOfMember)
-    
+
+    const handleClick = (type: string) => {
+        if(type === 'none'){
+            // call to API to join group
+        }
+        else if(type === 'member'){
+            // redirect to group page
+        }
+    }
+
     return (
-        <button className={`${membership.color} text-white px-4 py-2 rounded-lg`}>{membership.text}</button>
+        <button 
+            className={`${membership.color} text-white px-4 py-2 rounded-lg cursor-pointer`}
+            onClick={() => handleClick(membership.type)}
+        >{membership.text}</button>
     )
 }
--- src/features/groups/components/group-card.tsx
+++ src/features/groups/components/group-card.tsx
@@ -6,7 +6,7 @@ export const GroupCard = ({group} : {group: GroupResponse}) => {
             <div className="flex justify-center items-center flex-col h-[150px] w-[350px] border-[#7e007e] border-[1px] rounded-2xl m-4">
                 <h1 className="text-2xl font-bold">{group.name}</h1>
                 <h3>{group.category}</h3>
-                <GroupButton typeOfMember={group.membership} />
+                <GroupButton typeOfMember={group.membership} groupId={group.id} />
             </div>
     )
 }
