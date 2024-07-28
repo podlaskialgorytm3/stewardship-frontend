67b554a3fbc85c44b49778d3b1b312354e6363d5
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-28 20:54:03 +0200

    feat: create custom hook related with creating group

---
+++ src/features/groups/api/use-create-group.ts
@@ -0,0 +1,44 @@
+import { useMutation } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+import { queryClient } from "../../../api/utils/query-client";
+
+import Swal from "sweetalert2";
+
+const createGroup = async ({name, category} : {name: string, category: string}) => {
+    const response = await fetch(`${API_URL}/stewardship/group`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        },
+        body: JSON.stringify({name, category})
+    })
+
+    if(!response.ok){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!"
+        })
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+const useCreateGroup = () => (
+    useMutation({
+        mutationFn: createGroup,
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({queryKey: ["groups"]}),
+            Swal.fire({
+                icon: data.type,
+                title: data.type,
+                text: data.message
+            })
+        }
+    })
+)
+
+export default useCreateGroup;
