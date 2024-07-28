035a75b025b0263322b3f5b9632bc447eb38a1a6
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-28 21:07:48 +0200

    style: change margin in group card

--- src/features/groups/components/group-card.tsx
+++ src/features/groups/components/group-card.tsx
@@ -5,7 +5,7 @@ export const GroupCard = ({group} : {group: GroupResponse}) => {
     return(
             <div className="flex justify-center items-center flex-col h-[150px] w-[350px] border-[#7e007e] border-[1px] rounded-2xl m-4">
                 <h1 className="text-2xl font-bold">{group.name}</h1>
-                <h3>{group.category}</h3>
+                <h3 className="mb-2">{group.category}</h3>
                 <GroupButton typeOfMember={group.membership} groupId={group.id} />
             </div>
     )
