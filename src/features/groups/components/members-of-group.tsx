74e590a95e3b4ce8baf83a3e77ace13d0d8a4b7c
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-29 20:20:41 +0200

    feat: create simple look of member component

--- src/features/groups/components/members-of-group.tsx
+++ src/features/groups/components/members-of-group.tsx
@@ -9,10 +9,18 @@ export const MembersOfGroup: React.FC<{groupId: string | undefined}> = ({groupId
     
 
     return(
-        <div className="w-[30%] border-[2px] border-[#7e007e] rounded-xl flex flex-col items-center">
-                <h1 className="text-xl font-bold">members-of-group</h1>
+        <div className="w-[30%] border-[2px] h-[400px] overflow-y-scroll border-[#7e007e] rounded-xl flex flex-col items-center">
+                <h1 className="text-xl font-bold mt-5">members-of-group</h1>
                 <SearchBar placeholder="Search members" search={search} setSearch={setSearch} />
-                
+                {!isLoading && data.data && data.data.map((member: any) => {
+                    return (
+                        <div key={member.id} className="flex items-center justify-between w-[90%] p-2 rounded-xl cursor-pointer hover:bg-[#7e007e] hover:text-white">
+                            <img src={member.img} alt={member.name} className="w-[50px] h-[50px] rounded-full object-cover border-[#7e007e] border-[1px]" />
+                            <p>{member.name}</p>
+                            <p>{member.role}</p>
+                        </div>
+                    )
+                })}
         </div>
     )
 } 
