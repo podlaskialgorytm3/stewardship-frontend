702dde408417f58ce6f7a73359f8ed148501d92a
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-29 20:44:17 +0200

    feat: create custom function which is called to api to get response if user is admin of group

---
+++ src/features/members/api/use-check-role.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+
+import Swal from "sweetalert2";
+
+const checkRole = async (groupId: string) => {
+    const response = await fetch(`${API_URL}/stewardship//group-user/is-admin/${groupId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+    })
+
+    if(!response.ok){
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Something went wrong"
+        })
+    }
+
+    const data = await response.json(); // response true or false
+
+    return data;
+}
+
+const useCheckRole = (groupId: string) => (
+    useQuery({
+        queryFn: () => checkRole(groupId),
+        queryKey: ["groups", groupId]
+    })
+)
+
+export default useCheckRole;
