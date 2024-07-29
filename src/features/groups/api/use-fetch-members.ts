e65238505f5fbb5adb996352debe23c2bf81babb
podlaskialgorytm3 <live.m851@gmail.com>
2024-07-29 19:31:10 +0200

    fix: change type of sending data to API

--- src/features/groups/api/use-fetch-members.ts
+++ src/features/groups/api/use-fetch-members.ts
@@ -3,13 +3,12 @@ import { API_URL } from "../../../shared/constants/constants";
 import Swal from "sweetalert2";
 
 const fetchMembers = async ({groupId, username}: {groupId: string, username: string}) => {
-    const response = await fetch(`${API_URL}/stewardship/group-user`,{
-        method: 'POST',
+    const response = await fetch(`${API_URL}/stewardship/group-user?groupId=${groupId}&username=${username}`,{
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${localStorage.getItem('token')}`
-        },
-        body: JSON.stringify({groupId, username})
+        }
     })
 
     if(!response.ok){
--- src/features/groups/components/members-of-group.tsx
+++ src/features/groups/components/members-of-group.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react"
 
 import { SearchBar } from "../../search-bar/components/search-bar"
+import useFetchMembers from "../api/use-fetch-members";
 
 export const MembersOfGroup: React.FC<{groupId: string | undefined}> = ({groupId}) => {
     const [search, setSearch] = useState<string>("");
-
+    const { data, isLoading } = useFetchMembers({groupId: groupId as string, username: search as string});
+    
 
     return(
         <div className="w-[30%] border-[2px] border-[#7e007e] rounded-xl flex flex-col items-center">
                 <h1 className="text-xl font-bold">members-of-group</h1>
                 <SearchBar placeholder="Search members" search={search} setSearch={setSearch} />
+                
         </div>
     )
 } 
