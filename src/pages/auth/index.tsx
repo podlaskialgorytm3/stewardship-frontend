bbfa35f285030a2b5c530819576702f9cf75ea25
podlaskialgorytm3 <live.m851@gmail.com>
2024-08-05 19:49:54 +0200

    feat: create unique auth page for authenticate users

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import useAuth from "../../api/hooks/use-auth";
+import { AuthError } from "../../shared/components/auth-error";
+
+
+const AuthPage: React.FC<{children: JSX.Element}> = ({children}) => {
+    const { data, isLoading } = useAuth();
+
+    return (
+        <div>
+            {(!data?.authenticated && !isLoading) ? 
+                <AuthError>
+                    You are not logged in!
+                </AuthError>
+                :
+                <>
+                    {children}
+                </>
+            }
+        </div>
+    );
+}
+
+export default AuthPage;
