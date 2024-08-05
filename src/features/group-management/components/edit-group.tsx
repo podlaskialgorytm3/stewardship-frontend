ae8bd76ae8bc859dcc0fbecac7b2741c88e993d5
podlaskialgorytm3 <live.m851@gmail.com>
2024-08-05 21:49:44 +0200

    feat: create simple form before edit group

--- src/features/group-management/components/edit-group.tsx
+++ src/features/group-management/components/edit-group.tsx
@@ -1,12 +1,97 @@
 import { useParams } from "react-router-dom"
+import useCheckRole from "../../../api/hooks/use-check-role";
+import useFetchGroup from "../api/use-fetch-group";
+import useErrorMessage from "../../../shared/hooks/use-error-message";
+import { AuthError } from "../../../shared/components/auth-error";
+import Loading  from "../../../shared/components/loading";
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { ThemeProvider } from '@mui/material/styles';
+import { defaultTheme } from '../../../shared/themes/themes';
 
 export const EditGroup: React.FC = () => {
     const { id } = useParams<{id: string}>();
+    const {data: isAdmin, isLoading: roleLoading } = useCheckRole(id as string) 
+    const { data, isLoading, isError, error } = useFetchGroup(id as string);
 
+    const isPending = false
+
+    useErrorMessage({isError, error } as {isError: boolean, error: {message: string}});
     return (
-        <div>
-            <h1>Edit Group</h1>
-            {id}
-        </div>
+        <>
+            {
+                !isLoading && !roleLoading && isAdmin && data ? (
+                  
+                        <ThemeProvider theme={defaultTheme}>
+                            <Container component="main" maxWidth="xs" sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                            {isPending ? 
+                            <>
+                                <Loading size={100} />
+                            </>
+                            :
+                                <Box
+                                sx={{
+                                    marginTop: 8,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                }}
+                                >
+                                <h1 className='text-3xl font-bold'>Edit group</h1> 
+                                <Box component="form" noValidate sx={{ mt: 1 }} >
+                                    <TextField
+                                        margin="normal"
+                                        defaultValue={data?.name}
+                                        required
+                                        fullWidth
+                                        id="name"
+                                        label="group-name"
+                                        name="name"
+                                        autoFocus
+                                        //onChange={() => handleChange('name')}
+                                        //helperText={formErrors.name}
+                                        //error={Boolean(formErrors.name)}
+                                    />
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="category"
+                                        label="group-category"
+                                        defaultValue={data?.category}
+                                        type="text"
+                                        id="category"
+                                        //onChange={() => handleChange('category')}
+                                        //helperText={formErrors.category}
+                                        //error={Boolean(formErrors.category)}
+                                    />
+                                    <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    >
+                                    Edit group
+                                    </Button>
+                                </Box>
+                                </Box>
+                                }
+                            </Container>  
+                    </ThemeProvider>
+                ) : (
+                    <AuthError>
+                        You are not authorized to edit this group
+                    </AuthError>
+                )
+            }
+        </>
     )
 }
--- src/features/group-management/components/group-management.tsx
+++ src/features/group-management/components/group-management.tsx
@@ -1,12 +1,13 @@
 import useFetchGroup from "../api/use-fetch-group";
 import Loading from "../../../shared/components/loading";
 import useErrorMessage from "../../../shared/hooks/use-error-message";
+import useCheckRole from "../../../api/hooks/use-check-role";
 
 import { ButtonArea } from "./button-area";
 
 export const GroupManagement: React.FC<{groupId: string | undefined}> = ({groupId}) => {
     const { data, isLoading, isError, error } = useFetchGroup(groupId as string);
-
+    const { data: isAdmin, isLoading: roleLoading } = useCheckRole(groupId as string);
   
     useErrorMessage({isError, error } as {isError: boolean, error: {message: string}});
     return (
@@ -19,7 +20,7 @@ export const GroupManagement: React.FC<{groupId: string | undefined}> = ({groupI
             ): (
                 <Loading size={50} />
             )}
-            <ButtonArea groupId={groupId}/>
+            {!roleLoading && isAdmin && <ButtonArea groupId={groupId} />}
         </div>
     );
 }
