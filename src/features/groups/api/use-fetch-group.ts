9a8d2cb35fda55a7822db8194e011e4fc8748eb4
podlaskialgorytm3 <live.m851@gmail.com>
2024-08-05 21:08:24 +0200

    feat: create custom hook which called to API

---
+++ src/features/groups/api/use-fetch-group.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+import Swal from "sweetalert2";
+
+const fetchGroup = async (id: string) => {
+    const response = await fetch(`${API_URL}/stewardship/group/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+    if (!response.ok) {
+        const message = await response.text();
+        Swal.fire({
+            icon:'error',
+            title: "Oops...",
+            text: message
+        })
+    }
+    
+    const data = await response.json();
+
+    return data.data as {
+        name: string,
+        category: string
+    };
+}
+
+const useFetchGroup = (id: string) => (
+    useQuery({
+        queryKey: ['groups',id],
+        queryFn: () => fetchGroup(id)
+    })
+)
+
+export default useFetchGroup;
