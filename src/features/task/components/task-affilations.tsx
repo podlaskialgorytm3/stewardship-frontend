811a937bde582c6e47a1f54304ca4fd66cbfdbdb
podlaskialgorytm <live.m851@gmail.com>
2024-08-09 14:17:55 +0200

    feat: create new call to API and fetch users without admin

--- src/features/task/components/task-affilations.tsx
+++ src/features/task/components/task-affilations.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import useFetchMembers from "../../members/api/use-fetch-members";
 import { Member } from "../../members/types/types";
 import { SearchBar } from "../../search-bar/components/search-bar";
 import { defaultTheme } from "../../../shared/themes/themes";
@@ -8,6 +7,8 @@ import { defaultTheme } from "../../../shared/themes/themes";
 import { Button } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import { Checkbox } from "@mui/material";
+
+import useFetchMembers from "../api/use-fetch-members";
 
 export const TaskAffilation: React.FC<{groupId: string | undefined, handleBack: () => void, handleCheck: (memberId: number) => void; checked: { memberId: number, check: boolean }[]}> 
     = ({groupId, handleBack, handleCheck, checked}) => {
@@ -24,6 +25,7 @@ export const TaskAffilation: React.FC<{groupId: string | undefined, handleBack: 
                 return (
                     <div 
                         className="flex items-center justify-between w-[90%] p-2 rounded-xl cursor-pointer hover:bg-[#7e007e] hover:text-white"
+                        key={member.id}
                         >
                         <img src={member.img} alt={member.name} className="w-[50px] h-[50px] rounded-full object-cover border-[#7e007e] border-[1px]" />
                         <p>{member.name}</p>
