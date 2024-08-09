0cc9908455f1964489ab121591648d8356d60aad
podlaskialgorytm <live.m851@gmail.com>
2024-08-09 11:53:28 +0200

    feat: create validation with zod

--- src/features/task/utils/utils.ts
+++ src/features/task/utils/utils.ts
@@ -1,4 +1,6 @@
-import { object, string } from 'zod';
+import { date, datetimeRegex, object, string } from 'zod';
+
+const datatimeLocalRegex = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})$/;
 
 export const subtaskSchema = object({
   title: string({
@@ -11,3 +13,20 @@ export const subtaskSchema = object({
         required_error: 'Status is required'
     }).min(3, 'Status must be at least 3 characters'),
 });
+
+export const taskSchema = object({
+  'task-name': string({
+    message: 'Title is required'
+  }).min(3, 'Title must be at least 3 characters'),
+  'start-date': string().regex(datatimeLocalRegex, 'Invalid date format'),
+  'end-date': string().regex(datatimeLocalRegex, 'Invalid date format'),
+   status: string({
+      message: 'Status is required'
+    }).min(3, 'Status must be at least 3 characters'),
+   priority: string({
+      message: 'Priority is required'
+    }).min(3, 'Priority must be at least 3 characters'),
+   comments: string({
+      message: 'Comments is required'
+    }).min(3, 'Comments must be at least 3 characters')
+})
