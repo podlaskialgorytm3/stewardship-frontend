1bda0fefd579214ae29f515337a98fb3180f3dda
podlaskialgorytm <live.m851@gmail.com>
2024-08-12 20:36:33 +0200

    feat: create types for data from response

--- src/features/task/types/types.ts
+++ src/features/task/types/types.ts
@@ -15,3 +15,40 @@ export interface TaskAffilationInterface {
     memberId: number;
     check: boolean;
 }
+
+interface Member {
+    email: string;
+    group: string;
+    id: string;
+    img: string;
+    name: string;
+    role: string;
+}
+
+interface TaskInfoResponse {
+    assignedBy: Member;
+    comments: string;
+    endDate: string;
+    startDate: string;
+    id: string,
+    name: string;
+    priority: string;
+    status: string;
+    time: number;
+}
+
+interface SubtaskResponse {
+    assignedBy: Member;
+    description: string;
+    id: string;
+    status: string;
+    title: string;
+    taskInfoId: string;
+}
+
+export interface TaskCardInterface {
+    taskInfo: TaskInfoResponse;
+    subtasks: SubtaskResponse[];
+    taskAffiliation: Member[];
+    precentOfDoneSubtasks: string;
+}
