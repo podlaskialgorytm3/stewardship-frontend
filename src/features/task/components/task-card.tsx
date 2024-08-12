76ce439a7ad78bd9c7c782c029086b0768e1b616
podlaskialgorytm <live.m851@gmail.com>
2024-08-12 21:33:08 +0200

    feat: create task card

--- src/features/task/components/task-card.tsx
+++ src/features/task/components/task-card.tsx
@@ -2,68 +2,43 @@ import { ThemeProvider } from "@emotion/react";
 import { defaultTheme } from "../../../shared/themes/themes";
 import { Button } from "@mui/material";
 
-export const TaskCard = () => {
+import { TaskCardInterface } from "../types/types";
+
+import { TaskCardAvatars } from "./task-card-avatars";
+
+import { convertHoursToTime, formatDateTime } from "../utils/utils";
+
+export const TaskCard = ({task} : {task: TaskCardInterface}) => {
   return (
     <ThemeProvider theme={defaultTheme}>
     <div className="border-primary border-[2px] text-primary rounded-xl p-6 shadow-md relative w-[700px] m-3">
-      {/* Title */}
       <div className="flex justify-between">
-            <h3 className="text-2xl font-semibold mb-4">name-of-task</h3>
+            <h3 className="text-2xl font-semibold mb-4">{task.taskInfo.name}</h3>
             <p className="flex justify-center items-center">
             <span>assigned-by</span>
             <img 
                 className="w-10 h-10 rounded-full border-2 border-dark m-3" 
-                src="https://via.placeholder.com/40" 
-                alt="Avatar 1" 
+                src={task.taskInfo.assignedBy.img} 
+                alt={task.taskInfo.assignedBy.name}
             />
             </p>
       </div>
-      
-      {/* Avatars */}
-      <div className="flex items-center mb-4">
-        <div className="flex -space-x-3">
-          <img 
-            className="w-10 h-10 rounded-full border-2 border-dark" 
-            src="https://via.placeholder.com/40" 
-            alt="Avatar 1" 
-          />
-          <img 
-            className="w-10 h-10 rounded-full border-2 border-dark" 
-            src="https://via.placeholder.com/40" 
-            alt="Avatar 2" 
-          />
-          <img 
-            className="w-10 h-10 rounded-full border-2 border-dark" 
-            src="https://via.placeholder.com/40" 
-            alt="Avatar 3" 
-          />
-          <div className="w-10 h-10 rounded-full bg-primary text-white flex items-center justify-center text-sm border-2 border-dark">
-            +3
-          </div>
-        </div>
-      </div>
-
-      {/* Icons and info */}
+      <TaskCardAvatars members={task.members} />
       <div className="flex items-center justify-between text-primary mb-4">
         <div className="flex space-x-4">
           <div className="flex items-center space-x-1">
-            <span>📝 23</span>
+            <span>{task.subTasks.length} 📝</span>
           </div>
-
           <div className="flex items-center space-x-1">
-            <span>⏰ 03:23:20</span>
+            <span>⏰ {convertHoursToTime(task.taskInfo.time)}</span>
           </div>
         </div>
       </div>
-
-      {/* Progress bar */}
       <div className="relative h-2 rounded-full bg-ligth mb-2">
-        <div className="absolute top-0 left-0 h-2 rounded-full bg-dark" style={{ width: '40%' }}></div>
+        <div className="absolute top-0 left-0 h-2 rounded-full bg-dark" style={{ width: `${task.precentOfDoneSubtasks}` }}></div>
       </div>
-      
-      {/* Estimated time */}
       <div className="text-primary text-sm mt-4 flex w-full justify-between">
-        <p> 15.03.2022 8:00 - 15.03.2023 9:00 </p>
+        <p>{formatDateTime(task.taskInfo.startDate)} - {formatDateTime(task.taskInfo.endDate)}</p>
         <Button variant="contained" color="primary">View Task</Button>
       </div>
     </div>
