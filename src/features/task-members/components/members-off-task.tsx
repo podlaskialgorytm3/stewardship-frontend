257c7b2e548c89288c4b6a3bc7e904ef0c43cf6c
podlaskialgorytm <live.m851@gmail.com>
2024-08-13 12:27:18 +0200

    feat: create components where will be show users, which will added to task and will not added

---
+++ src/features/task-members/components/members-off-task.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+import { Member } from "../types/types";
+import { MemberCard } from "./member-card";
+import { SearchBar } from "../../search-bar/components/search-bar";
+
+export const MembersOffTask: React.FC<{groupId: string | undefined}> = ({groupId}) => {
+    const [search, setSearch] = useState<string>("");
+
+    return(
+        <div className="flex flex-col items-center">
+            <h1 className="text-xl font-bold mt-5">members-off-task</h1>
+            <SearchBar placeholder="Search requests" search={search} setSearch={setSearch} />
+            {/* {!isLoading && data.data && data.data.map((member: Member) => {
+            return (
+                <MemberCard member={member} groupId={groupId} key={member.id} type="waiting"/>
+            )
+            })}   */}
+        </div>
+    )
+}
