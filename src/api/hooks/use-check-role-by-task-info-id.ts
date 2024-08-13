2d9674397f1fe0dd6e942e7260f1b4bfb33c616b
podlaskialgorytm <live.m851@gmail.com>
2024-08-13 12:24:00 +0200

    feat: create call to API to check role if user is admin by taskInfoId

---
+++ src/api/hooks/use-check-role-by-task-info-id.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../shared/constants/constants";
+
+import Swal from "sweetalert2";
+
+const checkRoleByTaskInfoId = async (taskInfoId: string) => {
+    const response = await fetch(`${API_URL}/stewardship/task-info/is-admin/${taskInfoId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+    })
+
+    if(!response.ok){
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Something went wrong"
+        })
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+const useCheckRoleByTasInfokId = (taskInfoId: string) => (
+    useQuery({
+        queryFn: () => checkRoleByTaskInfoId(taskInfoId),
+        queryKey: ["groups", taskInfoId]
+    })
+)
+
+export default useCheckRoleByTasInfokId;
