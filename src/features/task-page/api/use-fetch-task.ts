bc40edbe4de5eec833df3943de6311b770c1b0f2
podlaskialgorytm <live.m851@gmail.com>
2024-08-13 13:07:00 +0200

    feat: create custom hook which is called to API to get information about task

---
+++ src/features/task-page/api/use-fetch-task.ts
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+import Swal from "sweetalert2";
+
+import { TaskPageResponse } from "../types/types";
+
+const fetchTask = async (taskInfoId: string) => {
+    const response = await fetch(`${API_URL}/stewardship/task-info/${taskInfoId}`,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+
+    if(!response.ok){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'something-went-wrong'
+        })
+    }
+
+    const data = await response.json();
+
+    return data as TaskPageResponse;
+}
+
+const useFetchTask = (taskInfoId: string) => (
+    useQuery({
+        queryKey: ['task', taskInfoId],
+        queryFn: () => fetchTask(taskInfoId)
+    })
+)
+
+export default useFetchTask;
