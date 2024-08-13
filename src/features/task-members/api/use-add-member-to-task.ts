37de18a45ad3742ff090f4a432703f6cc7519ce9
podlaskialgorytm <live.m851@gmail.com>
2024-08-13 23:46:16 +0200

    feat: create custom hook, which is called to API added member to task

---
+++ src/features/task-members/api/use-add-member-to-task.ts
@@ -0,0 +1,47 @@
+import { useMutation } from '@tanstack/react-query';
+import { API_URL } from '../../../shared/constants/constants';
+import { queryClient } from '../../../api/utils/query-client';
+
+import Swal from 'sweetalert2';
+
+const addMemberToTask = async ({memberId, taskInfoId} : {memberId: string, taskInfoId: string}) => {
+    const response = await fetch(`${API_URL}/stewardship/task-affilation`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify({
+            memberId,
+            taskInfoId
+        })
+    })
+
+    if(!response.ok){
+        Swal.fire({
+            title: 'Error',
+            text: 'An error occurred while adding the member to the task',
+            icon: 'error'
+        })
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+const useAddMemberToTask = () => (
+    useMutation({
+        mutationFn: addMemberToTask,
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({queryKey: ["members-added-to-task"]})
+            Swal.fire({
+                title: data.type,
+                text: data.message,
+                icon: data.type
+            })
+        }
+    })
+)
+
+export default useAddMemberToTask;
