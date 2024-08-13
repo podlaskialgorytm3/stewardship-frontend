d30ca4cac3bc7236f9e36949daa496bca17f2e82
podlaskialgorytm <live.m851@gmail.com>
2024-08-13 12:27:46 +0200

    feat: create single page to task and type necesery for members

---
+++ src/features/task-members/types/types.ts
@@ -0,0 +1,8 @@
+export interface Member {
+    email: string;
+    group: string;
+    id: string;
+    img: string;
+    name: string;
+    role: string;
+}
--- src/features/task-page/components/single-task-page.tsx
+++ src/features/task-page/components/single-task-page.tsx
@@ -1,7 +1,18 @@
+import { useParams } from "react-router-dom"
+
+import { TaskMembers } from "../../task-members/components/task-members"
+
 export const SingleTaskPage: React.FC = () => {
+    const { id } = useParams();
+
     return (
-        <div>
-            <h1>Single Task Page</h1>
+        <div className="mt-5 w-[1300px] flex">
+            <div className="w-[70%]">
+
+            </div>
+            <div className="w-[30%]">
+                <TaskMembers taskInfoId={id}/>
+            </div>
         </div>
     )
 }
