6fc79a8539b783e8992d8f35f6c03d15dc314552
podlaskialgorytm <live.m851@gmail.com>
2024-08-14 00:14:25 +0200

    feat: create call to custom hook and mutate data

--- src/features/task-members/hooks/use-handle-deleting.ts
+++ src/features/task-members/hooks/use-handle-deleting.ts
@@ -2,7 +2,15 @@ import { Member } from "../types/types";
 
 import Swal from "sweetalert2";
 
+import useDeleteMemberFromTask from "../api/use-delete-member-from-task";
+
+import useErrorMessage from "../../../shared/hooks/use-error-message";
+
 const useHandleDeleting = ({member, taskInfoId} : {member: Member, taskInfoId: string | undefined}) => {
+    const { mutate, isPending, isError, error } = useDeleteMemberFromTask();
+
+    useErrorMessage({isError, error} as {isError: boolean, error: {message: string}});
+
     const handleDeleteMemberFromTask = () => {
         Swal.fire({
             title: `Delete ${member.name} from this task?`,
@@ -14,7 +22,13 @@ const useHandleDeleting = ({member, taskInfoId} : {member: Member, taskInfoId: s
             showConfirmButton: true,
         }).then((result) => {
             if (result.isConfirmed) {
-            // mutate
+                isPending && Swal.fire({
+                    text: "Loading...",
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    showConfirmButton: false,
+                })
+                mutate({memberId: member.id, taskInfoId})
             }
         })
     }
