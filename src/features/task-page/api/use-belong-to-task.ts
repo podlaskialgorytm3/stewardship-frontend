0eb1f234ec62a1030d28b79820a5753a08702bae
podlaskialgorytm <live.m851@gmail.com>
2024-08-14 12:58:18 +0200

    feat: create custom hook to check if given user belong to given task

---
+++ src/features/task-page/api/use-belong-to-task.ts
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+import Swal from "sweetalert2";
+
+const belongToTask = async (taskInfoId: string) => {
+    const response = await fetch(`${API_URL}/stewardship/task-info/is-belong-to-task/${taskInfoId}`,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+
+    if(!response.ok) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+        });
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+const useBelongToTask = (taskInfoId: string) => (
+    useQuery({
+        queryKey: ['tasks', taskInfoId],
+        queryFn: () => belongToTask(taskInfoId)
+    })
+)
+
+export default useBelongToTask;
--- src/features/task-page/components/single-task-page.tsx
+++ src/features/task-page/components/single-task-page.tsx
@@ -5,10 +5,12 @@ import { TaskMembers } from "../../task-members/components/task-members"
 import useFetchTask from "../api/use-fetch-task";
 import Loading from "../../../shared/components/loading";
 
+import useErrorMessage from "../../../shared/hooks/use-error-message";
+
 export const SingleTaskPage: React.FC = () => {
     const { id } = useParams();
     const { data, isLoading, isError, error } = useFetchTask(id as string);
-
+    useErrorMessage({isError, error} as {isError: boolean, error: {message: string}});
     return (
         <div className="mt-5 w-[1300px] flex">
             {isLoading && !data && <Loading size={150}/>}
