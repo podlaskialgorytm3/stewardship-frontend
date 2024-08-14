908bdb61d92e239675cb01dddd882071be38d2ee
podlaskialgorytm <live.m851@gmail.com>
2024-08-14 13:01:42 +0200

    feat: handle error realted with belonging given user to task

--- src/features/task-page/components/single-task-page.tsx
+++ src/features/task-page/components/single-task-page.tsx
@@ -6,24 +6,34 @@ import useFetchTask from "../api/use-fetch-task";
 import Loading from "../../../shared/components/loading";
 
 import useErrorMessage from "../../../shared/hooks/use-error-message";
+import useBelongToTask from "../api/use-belong-to-task";
+import { AuthError } from "../../../shared/components/auth-error";
 
 export const SingleTaskPage: React.FC = () => {
     const { id } = useParams();
     const { data, isLoading, isError, error } = useFetchTask(id as string);
+    const { data: belongToTask, isLoading: loadingMemberBelongToTask } = useBelongToTask(id as string);
+
     useErrorMessage({isError, error} as {isError: boolean, error: {message: string}});
     return (
-        <div className="mt-5 w-[1300px] flex">
-            {isLoading && !data && <Loading size={150}/>}
-            {!isLoading && data && (
-                <>
-                    <div className="w-[70%]">
-
-                    </div>
-                    <div className="w-[30%]">
-                        <TaskMembers taskInfoId={id}/>
-                    </div>
-                </>
+        <>
+            {loadingMemberBelongToTask && <Loading size={150} />}
+            {!loadingMemberBelongToTask && belongToTask && (
+                <div className="mt-5 w-[1300px] flex">
+                {isLoading && !data && <Loading size={150}/>}
+                {!isLoading && data && (
+                    <>
+                        <div className="w-[70%]">
+    
+                        </div>
+                        <div className="w-[30%]">
+                            <TaskMembers taskInfoId={id}/>
+                        </div>
+                    </>
+                )}
+            </div>
             )}
-        </div>
+            {!loadingMemberBelongToTask && !belongToTask && <AuthError> You are not belong to this task! </AuthError>}
+        </>
     )
 }
