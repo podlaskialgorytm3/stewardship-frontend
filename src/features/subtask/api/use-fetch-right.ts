7ee64304550a77ef05cc5c83a2a4e1596d79a0a0
podlaskialgorytm <live.m851@gmail.com>
2024-08-15 13:33:19 +0200

    feat: create custom hook to get rights if user can handle given subtask

---
+++ src/features/subtask/api/use-fetch-right.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+
+import Swal from "sweetalert2";
+
+export const fetchRight = async (subtaskId: string) => {
+    const response = await fetch(`${API_URL}/stewardship/subtask/has-right/${subtaskId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+    })
+
+    if(!response.ok){
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Something went wrong"
+        })
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+const useFetchRight = (subtaskId: string) => (
+    useQuery({
+        queryKey: ["rights", subtaskId],
+        queryFn: () => fetchRight(subtaskId)
+    })
+)
+
+export default useFetchRight;
