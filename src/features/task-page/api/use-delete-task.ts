a9ef1cfa91d2f6658a3188d8ba05669b780f0419
podlaskialgorytm <live.m851@gmail.com>
2024-08-20 13:35:38 +0200

    feat: create custom hook to call to API to deleting task

---
+++ src/features/task-page/api/use-delete-task.ts
@@ -0,0 +1,52 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
+import { API_URL } from "../../../shared/constants/constants";
+import { queryClient } from "../../../api/utils/query-client";
+
+import Swal from "sweetalert2";
+
+const deleteTask = async (taskInfoId: string) => {
+  const response = await fetch(
+    `${API_URL}/stewardship/task-info/${taskInfoId}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    Swal.fire({
+      title: "Error",
+      text: "Something went wrong",
+      icon: "error",
+    });
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+const useDeleteTask = () => {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: deleteTask,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["task"] });
+      Swal.fire({
+        icon: data.type,
+        title: data.type,
+        text: data.message,
+      });
+      setTimeout(() => {
+        navigate(-1);
+      }, 1000);
+    },
+  });
+};
+
+export { useDeleteTask };
