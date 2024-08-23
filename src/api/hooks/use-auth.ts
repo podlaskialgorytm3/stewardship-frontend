fad4ca2c2f59b338d5e3943ce1d1af48cf068246
podlaskialgorytm <live.m851@gmail.com>
2024-08-23 22:04:44 +0200

    reactor: change hook to checking authorization

--- src/api/hooks/use-auth.ts
+++ src/api/hooks/use-auth.ts
@@ -1,38 +1,37 @@
-import  Swal  from 'sweetalert2';
-import { useQuery } from '@tanstack/react-query';
-import { API_URL } from '../../shared/constants/constants';
+import Swal from "sweetalert2";
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../shared/constants/constants";
 
-const fetchToken = async (token: string) => {
-    const response = await fetch(`${API_URL}/stewardship/user/token/validate`, {
-        method: 'GET',
-        headers: {
-            'authorization': `Bearer ${token}`
-        }
+const fetchToken = async () => {
+  const response = await fetch(`${API_URL}/stewardship/user/token/validate`, {
+    method: "GET",
+    headers: {
+      authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+
+  if (!response.ok) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
     });
-    
-    if (!response.ok) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Something went wrong!',
-        });
-        throw new Error('Something went wrong');
-    }
+    throw new Error("Something went wrong");
+  }
 
-    const data = await response.json();
+  const data = await response.json();
 
-    const now = new Date().getTime();
-    const expiresIn = localStorage.getItem('expiresIn');   
+  const now = new Date().getTime();
+  const expiresIn = localStorage.getItem("expiresIn");
 
-    return data?.authenticated && now < Number(expiresIn) + 60 * 60 * 1000 + 24;
-}
+  return data?.authenticated && now < Number(expiresIn) + 60 * 60 * 1000 + 24;
+};
 
-const useAuth = () => {;
-    const token = localStorage.getItem('token');
-    return useQuery({
-        queryKey: ['user', token],
-        queryFn: () => fetchToken(token as string)
-    });
-}
+const useAuth = () => {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: () => fetchToken(),
+  });
+};
 
 export default useAuth;
