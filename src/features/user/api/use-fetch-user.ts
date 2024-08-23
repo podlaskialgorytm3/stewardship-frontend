eb5555bc5fa779ee9dc21fb99f0e6bc07faaf807
podlaskialgorytm <live.m851@gmail.com>
2024-08-23 23:45:21 +0200

    refactor: delete token from URI

--- src/features/user/api/use-fetch-user.ts
+++ src/features/user/api/use-fetch-user.ts
@@ -3,28 +3,28 @@ import { API_URL } from "../../../shared/constants/constants";
 import Swal from "sweetalert2";
 
 const fetchUser = async () => {
-    const response = await fetch(`${API_URL}/stewardship/user/${localStorage.getItem("token")}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem("token")}`
-        }
+  const response = await fetch(`${API_URL}/stewardship/user`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+
+  if (!response.ok) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
     });
+  }
 
-    if (!response.ok) {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Something went wrong!"
-        });
-    }
-
-    return response.json();
-}
+  return response.json();
+};
 
 export const useFetchUser = () => {
-    return useQuery({
-        queryKey: ["user"],
-        queryFn: () => fetchUser(),
-    })
-}
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: () => fetchUser(),
+  });
+};
