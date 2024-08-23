7c0d7574d8549706df8be33117ff6935df62780c
podlaskialgorytm <live.m851@gmail.com>
2024-08-23 22:43:10 +0200

    refactor: change way of logout, reduce logout to same token without email

--- src/features/login/api/use-logout.ts
+++ src/features/login/api/use-logout.ts
@@ -1,53 +1,50 @@
-import { useNavigate } from 'react-router-dom';
-import { UserData } from '../types/types';
-import Swal from 'sweetalert2';
-import { useMutation } from '@tanstack/react-query';
-import { queryClient } from '../../../api/utils/query-client';
-import { API_URL } from '../../../shared/constants/constants';
+import { useNavigate } from "react-router-dom";
+import { UserData } from "../types/types";
+import Swal from "sweetalert2";
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../../../api/utils/query-client";
+import { API_URL } from "../../../shared/constants/constants";
 
-const logout = async (email: string, token: string) => {
-    const response = await fetch(`${API_URL}/stewardship/user/logout`, {
-        method: 'POST',
-        mode: 'cors',
-        body: JSON.stringify(email),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    })
+const logout = async () => {
+  const response = await fetch(`${API_URL}/stewardship/user/logout`, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 
-    if(!response.ok){
-        return "Error";
-    }
+  if (!response.ok) {
+    return "Error";
+  }
 
-    const data = await response.json();
+  const data = await response.json();
 
-    return data;
-}
+  return data;
+};
 
-const useLogout =  () => {
-    const navigate = useNavigate();
-    const user: UserData | null | undefined | any = localStorage.getItem('user');
-    const token = localStorage.getItem('token');
-    const logoutMutation = useMutation({
-        mutationFn: () => logout(user.email, String(token)),
-        onSuccess: (data) => {
-            queryClient.invalidateQueries({queryKey: ['user']});
-            Swal.fire({
-                icon: data.type,
-                title: data.type.charAt(0).toUpperCase() + data.type.slice(1),
-                text: data.message
-            })
-            localStorage.removeItem('user');
-            localStorage.removeItem('token');
-            setTimeout(() => {
-                window.location.reload();
-                navigate('/login');
-            }, 1000)
-        }
-    });
-    
-    return logoutMutation;
-}
+const useLogout = () => {
+  const navigate = useNavigate();
+  const logoutMutation = useMutation({
+    mutationFn: () => logout(),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      Swal.fire({
+        icon: data.type,
+        title: data.type.charAt(0).toUpperCase() + data.type.slice(1),
+        text: data.message,
+      });
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      setTimeout(() => {
+        window.location.reload();
+        navigate("/login");
+      }, 1000);
+    },
+  });
+
+  return logoutMutation;
+};
 
 export default useLogout;
--- src/features/menu/components/logout-button.tsx
+++ src/features/menu/components/logout-button.tsx
@@ -1,20 +1,21 @@
-import  useLogout  from '../../../features/login/api/use-logout';
-import { useNavigate } from 'react-router-dom';
+import useLogout from "../../../features/login/api/use-logout";
+import { useNavigate } from "react-router-dom";
 
-export const LogoutButton: React.FC<{children: string}> = ({children}) => {
-    const { mutate } = useLogout();
-    const  navigate  = useNavigate();
-    const handleClick = () => {
-        mutate();
-        navigate('/')
-    }
-    return (
-        <li>
-            <button
-            className='text-purple-800 font-bold w-20 flex hover:text-white hover:bg-purple-600 px-3 py-2 rounded-3xl '
-            onClick={handleClick}
-            >{children}
-            </button>
-        </li>
-    )
-}
+export const LogoutButton: React.FC<{ children: string }> = ({ children }) => {
+  const { mutate } = useLogout();
+  const navigate = useNavigate();
+  const handleClick = () => {
+    mutate();
+    navigate("/");
+  };
+  return (
+    <li>
+      <button
+        className="text-purple-800 font-bold w-20 flex hover:text-white hover:bg-purple-600 px-3 py-2 rounded-3xl "
+        onClick={handleClick}
+      >
+        {children}
+      </button>
+    </li>
+  );
+};
