a196c91d70fe2dcc271e96282bf163fe0e7b7581
podlaskialgorytm <live.m851@gmail.com>
2024-08-23 21:31:42 +0200

    style: change style of one paragraph

--- src/features/dashboard/components/task-card.tsx
+++ src/features/dashboard/components/task-card.tsx
@@ -10,7 +10,7 @@ export const TaskCard: React.FC<{ task: TaskDashboardResponse }> = ({
     <div className="mt-5 w-[90%] h-[120px] border-primary border-[2px] border-solid rounded-lg flex flex-col items-center justify-center">
       <div>
         <h1 className="text-xl font-bold mt-2">{task.name}</h1>
-        <p className="text-md">{task.assignedBy.group}</p>
+        <p className="text-md text-center">{task.assignedBy.group}</p>
       </div>
       <button
         className="bg-primary text-white p-2 rounded-md mt-2 mb-2"
