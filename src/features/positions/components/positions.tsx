ee69714892d9d023410eafcd5d1537cd8791c7b6
podlaskialgorytm <live.m851@gmail.com>
2024-09-19 16:28:50 +0200

    feat: getting group id by react router dom

--- src/features/positions/components/positions.tsx
+++ src/features/positions/components/positions.tsx
@@ -1,7 +1,14 @@
+import { useParams } from "react-router-dom";
+
+import { Members } from "./members";
+
 const Positions: React.FC = () => {
+  const { id } = useParams<{ id: string | undefined }>();
+
   return (
-    <div className="flex w-[100%] justify-center">
+    <div className="flex w-[100%] justify-center items-center flex-col">
       <h1 className="mt-10 text-3xl font-bold">position-management</h1>
+      <Members groupId={id} />
     </div>
   );
 };
