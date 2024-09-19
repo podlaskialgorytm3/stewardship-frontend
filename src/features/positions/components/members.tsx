7d07781b916f121c3b04883298272d06fe433f9c
podlaskialgorytm <live.m851@gmail.com>
2024-09-19 16:20:55 +0200

    feat: fetch all members from group to  view them in position page

---
+++ src/features/positions/components/members.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import { useFetchMembers } from "../api/use-fetch-members";
+import { Member } from "../../../shared/types/members";
+import { SearchBar } from "../../search-bar/components/search-bar";
+import { MemberCard } from "./member-card";
+
+const Members: React.FC<{ groupId: string | undefined }> = ({ groupId }) => {
+  const [search, setSearch] = useState<string>("");
+  const { data, isLoading } = useFetchMembers({
+    groupId: groupId as string,
+    username: search as string,
+  });
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-xl font-bold mt-5">members-of-group</h1>
+      <SearchBar
+        placeholder="search-members"
+        search={search}
+        setSearch={setSearch}
+      />
+      {!isLoading &&
+        data.data &&
+        data.data.map((member: Member) => {
+          return <MemberCard member={member} key={member.id} type="follower" />;
+        })}
+    </div>
+  );
+};
+
+export { Members };
