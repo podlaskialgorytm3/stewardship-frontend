3e2013e812a2d58988d96111a1f2c8aecd778420
podlaskialgorytm <live.m851@gmail.com>
2024-09-19 23:57:42 +0200

    feat: call this custom hook in skill-container component

--- src/features/skills/components/skill-container.tsx
+++ src/features/skills/components/skill-container.tsx
@@ -1,12 +1,27 @@
 import { BoxElement } from "../../../shared/components/box-element";
+import { Loading } from "../../../shared/components/loading";
+import { SkillCard } from "./skill-card";
+import { useFetchSkills } from "../api/use-fetch-skills";
+
+import { SkillInterface } from "../types/types";
 
 const SkillContainer: React.FC<{ groupId: string | undefined }> = ({
   groupId,
 }) => {
+  const { data, isLoading } = useFetchSkills({ groupId });
+
   return (
     <>
       <BoxElement size={350}>
         <h1 className="text-xl font-bold">skills</h1>
+        <div className="h-[200px] overflow-scroll">
+          {isLoading && <Loading size={50} />}
+          {!isLoading &&
+            data &&
+            data?.map((skill: SkillInterface & { id: string }) => (
+              <SkillCard key={skill.id} skill={skill} />
+            ))}
+        </div>
       </BoxElement>
     </>
   );
