aba0a2581dfc3eb7cf4cf47a765f97845c58f69b
podlaskialgorytm <live.m851@gmail.com>
2024-09-20 09:56:21 +0200

    feat: create custom hook which is calling to API to fetch belonging skills

---
+++ src/features/skills/api/use-fetch-belonging-skills.ts
@@ -0,0 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+import Swal from "sweetalert2";
+
+const fetchBelongingSkills = async ({
+  groupUserId,
+}: {
+  groupUserId: number;
+}) => {
+  const response = await fetch(
+    `${API_URL}/stewardship/group-skill/belonging?groupUserId=${groupUserId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
+    });
+  }
+
+  const data = (await response.json()) as
+    | { id: string; skillName: string }[]
+    | [];
+
+  return data;
+};
+
+const useFetchBelongingSkills = ({ groupUserId }: { groupUserId: number }) =>
+  useQuery({
+    queryFn: () => fetchBelongingSkills({ groupUserId }),
+    queryKey: ["skills", groupUserId],
+  });
+
+export { useFetchBelongingSkills };
