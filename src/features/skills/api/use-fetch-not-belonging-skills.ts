836c5061b45b2c7bad90d2007c80a149acc3371c
podlaskialgorytm <live.m851@gmail.com>
2024-09-22 17:36:52 +0200

    feat: create custom hook to fetching not belonging skills to given user

---
+++ src/features/skills/api/use-fetch-not-belonging-skills.ts
@@ -0,0 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../../shared/constants/constants";
+import Swal from "sweetalert2";
+
+const fetchNotBelongingSkills = async ({
+  groupUserId,
+}: {
+  groupUserId: number;
+}) => {
+  const response = await fetch(
+    `${API_URL}/stewardship/group-skill/not-belonging?groupUserId=${groupUserId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
+    });
+  }
+
+  const data = (await response.json()) as
+    | { id: string; skillName: string }[]
+    | [];
+
+  return data;
+};
+
+const useFetchNotBelongingSkills = ({ groupUserId }: { groupUserId: number }) =>
+  useQuery({
+    queryFn: () => fetchNotBelongingSkills({ groupUserId }),
+    queryKey: ["skills", groupUserId],
+  });
+
+export { useFetchNotBelongingSkills };
