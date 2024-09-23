df475a74c8e1076cc87255f7f49940254f81a1ba
podlaskialgorytm <live.m851@gmail.com>
2024-09-24 00:37:17 +0200

    feat: call custom hook which is handling form

--- src/features/schedule-rules/components/create-schedule-rule.tsx
+++ src/features/schedule-rules/components/create-schedule-rule.tsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
+import { CreateScheduleRuleInterface } from "../types/types";
+import { useHandleCreateScheduleRule } from "../hooks/use-handle-create-schedule-rule";
+
 import { Box, Button } from "@mui/material";
 import { FormTextInput } from "./form-text-input";
 import { CREATE_SCHEDULE_RULE } from "../constants/constants";
 import { CreateSchedulePropsInterface } from "../types/types";
 
 const CreateScheduleRule: React.FC = () => {
+  const [scheduleRule, setScheduleRule] = useState<CreateScheduleRuleInterface>(
+    {
+      scheduleRuleName: "",
+      maxDailyHours: 0,
+      maxWeeklyHours: 0,
+      minRestBeetwenShifts: 0,
+      minWeeklyRest: 0,
+    }
+  );
+
+  const { onSubmit, register, errors, handleSubmit } =
+    useHandleCreateScheduleRule({
+      setScheduleRule,
+      groupId: "1",
+    });
   return (
     <div className="flex flex-col items-center mt-10">
       <h1 className="text-2xl font-bold mb-4">create-schedule-rule</h1>
       <Box
         component="form"
         color={"secondary"}
-        //onSubmit={handleSubmit(onSubmit as () => void)}
+        onSubmit={handleSubmit(onSubmit as () => void)}
         sx={{
           display: "flex",
           flexDirection: "column",
@@ -20,7 +40,17 @@ const CreateScheduleRule: React.FC = () => {
         }}
       >
         {CREATE_SCHEDULE_RULE.map((rule: CreateSchedulePropsInterface) => (
-          <FormTextInput key={rule.name} rule={rule} />
+          <FormTextInput
+            key={rule.name}
+            rule={rule}
+            register={register}
+            errors={errors}
+            value={
+              scheduleRule[
+                rule.name as keyof CreateScheduleRuleInterface
+              ] as string
+            }
+          />
         ))}
         <Button type="submit" variant="contained" color="secondary">
           Submit
