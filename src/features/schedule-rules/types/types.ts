950e36d265900b9a078a9c01a6af4931154a3818
podlaskialgorytm <live.m851@gmail.com>
2024-09-25 00:14:33 +0200

    feat: add next validation rules for creating schedule rule

--- src/features/schedule-rules/types/types.ts
+++ src/features/schedule-rules/types/types.ts
@@ -27,10 +27,22 @@ const CreateScheduleRuleSchema = object({
   scheduleRuleName: z
     .string()
     .min(3, { message: "Name must be at least 3 characters long" }),
-  maxDailyHours: z.number().min(1, { message: "Must be at least 1" }),
-  maxWeeklyHours: z.number().min(1, { message: "Must be at least 1" }),
-  minRestBeetwenShifts: z.number().min(1, { message: "Must be at least 1" }),
-  minWeeklyRest: z.number().min(1, { message: "Must be at least 1" }),
+  maxDailyHours: z
+    .number()
+    .min(1, { message: "Must be at least 1" })
+    .max(24, { message: "Must be at most 24" }),
+  maxWeeklyHours: z
+    .number()
+    .min(1, { message: "Must be at least 1" })
+    .max(168, { message: "Must be at most 168" }),
+  minRestBeetwenShifts: z
+    .number()
+    .min(1, { message: "Must be at least 1" })
+    .max(24, { message: "Must be at most 24" }),
+  minWeeklyRest: z
+    .number()
+    .min(1, { message: "Must be at least 1" })
+    .max(168, { message: "Must be at most 168" }),
 });
 
 export type {
