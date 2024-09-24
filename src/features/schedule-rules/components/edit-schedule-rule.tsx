c204b55a178cf91a67328a3ce893acd87d4977c0
podlaskialgorytm <live.m851@gmail.com>
2024-09-24 23:54:49 +0200

    feat: create custom hook to validate data with react hook form

--- src/features/schedule-rules/components/edit-schedule-rule.tsx
+++ src/features/schedule-rules/components/edit-schedule-rule.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   ScheduleRuleInterface,
   CreateSchedulePropsInterface,
@@ -9,17 +11,34 @@ import { Box, Button } from "@mui/material";
 
 import { FormTextInput } from "./form-text-input";
 
+import { useHandleEditScheduleRule } from "../hooks/use-handle-edit-schedule-rule";
+
 const EditScheduleRule: React.FC<{
   groupId: string | undefined;
   scheduleRule: ScheduleRuleInterface;
 }> = ({ groupId, scheduleRule }) => {
+  const [scheduleRuleData, setScheduleRule] =
+    useState<CreateScheduleRuleInterface>({
+      scheduleRuleName: scheduleRule.scheduleRuleName,
+      maxDailyHours: scheduleRule.maxDailyHours,
+      maxWeeklyHours: scheduleRule.maxWeeklyHours,
+      minRestBeetwenShifts: scheduleRule.minRestBeetwenShifts,
+      minWeeklyRest: scheduleRule.minWeeklyRest,
+    });
+
+  const { onSubmit, register, errors, handleSubmit } =
+    useHandleEditScheduleRule({
+      setScheduleRule,
+      groupId: groupId,
+      scheduleRuleId: scheduleRule.id,
+    });
   return (
     <div className="w-full flex justify-center items-center flex-col">
       <h1 className="text-2xl font-bold mb-4">edit-schedule-rule</h1>
       <Box
         component="form"
         color={"secondary"}
-        // onSubmit={handleSubmit(onSubmit as () => void)}
+        onSubmit={handleSubmit(onSubmit as () => void)}
         sx={{
           display: "flex",
           flexDirection: "column",
@@ -32,10 +51,10 @@ const EditScheduleRule: React.FC<{
           <FormTextInput
             key={rule.name}
             rule={rule}
-            //register={register}
-            //errors={errors}
+            register={register}
+            errors={errors}
             value={
-              scheduleRule[
+              scheduleRuleData[
                 rule.name as keyof CreateScheduleRuleInterface
               ] as string
             }
---
+++ src/features/schedule-rules/hooks/use-handle-edit-schedule-rule.ts
@@ -0,0 +1,48 @@
+import { useForm } from "react-hook-form";
+import { fromZodError } from "zod-validation-error";
+import {
+  CreateScheduleRuleInterface,
+  CreateScheduleRuleSchema,
+} from "../types/types";
+
+const useHandleEditScheduleRule = ({
+  setScheduleRule,
+  groupId,
+  scheduleRuleId,
+}: {
+  setScheduleRule: React.Dispatch<
+    React.SetStateAction<CreateScheduleRuleInterface>
+  >;
+  groupId: string | undefined;
+  scheduleRuleId: string;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: CreateScheduleRuleInterface) => {
+    try {
+      const dataValidate = CreateScheduleRuleSchema.parse({
+        maxDailyHours: Number(data.maxDailyHours),
+        maxWeeklyHours: Number(data.maxWeeklyHours),
+        minRestBeetwenShifts: Number(data.minRestBeetwenShifts),
+        minWeeklyRest: Number(data.minWeeklyRest),
+        scheduleRuleName: data.scheduleRuleName,
+      });
+      setScheduleRule(dataValidate);
+      // mutowanie danych
+    } catch (error: any) {
+      const validationError = fromZodError(error);
+      validationError.details.forEach((detail: any) => {
+        setError(detail.path[0], { type: "manual", message: detail.message });
+      });
+    }
+  };
+
+  return { onSubmit, register, errors, handleSubmit };
+};
+
+export { useHandleEditScheduleRule };
