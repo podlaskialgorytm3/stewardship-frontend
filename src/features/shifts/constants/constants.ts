dd5d60c70e3615a83e7961ee316425c1a891a8ec
podlaskialgorytm <live.m851@gmail.com>
2024-09-26 01:32:05 +0200

    feat: create const to getting information about single input in form

---
+++ src/features/shifts/constants/constants.ts
@@ -0,0 +1,11 @@
+const CREATE_SHIFT = [
+  { name: "nameOfShift", type: "text", label: "shift name" },
+  { name: "startFrom", type: "text", label: "start from" },
+  { name: "startTo", type: "text", label: "start to" },
+  { name: "endFrom", type: "text", label: "end from" },
+  { name: "endTo", type: "text", label: "end to" },
+  { name: "minDailyHours", type: "number", label: "min daily hours" },
+  { name: "maxDailyHours", type: "number", label: "max daily hours" },
+];
+
+export { CREATE_SHIFT };
