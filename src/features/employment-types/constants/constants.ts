100a2e3816252170f0cb454f0c4577ee9da0e42a
podlaskialgorytm <live.m851@gmail.com>
2024-10-02 01:16:34 +0200

    feat: create constant for inputs and their inputs

---
+++ src/features/employment-types/constants/constants.ts
@@ -0,0 +1,6 @@
+const CREATE_EMPLOYMENT_TYPES = [
+  { name: "employmentName", type: "text", label: "employment type" },
+  { name: "workingHours", type: "number", label: "wokring hours" },
+];
+
+export { CREATE_EMPLOYMENT_TYPES };
