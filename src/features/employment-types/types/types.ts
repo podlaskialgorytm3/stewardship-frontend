c130e99075e70bbdccd3c4bf04da7523e3f42e80
podlaskialgorytm <live.m851@gmail.com>
2024-10-02 01:15:56 +0200

    feat: create types, interfaces and schema to prepare data going to db

--- src/features/employment-types/types/types.ts
+++ src/features/employment-types/types/types.ts
@@ -1,7 +1,31 @@
+import { z, object } from "zod";
+
 interface EmploymentTypeInterface {
   id: string;
   employmentName: string;
   workingHours: number;
 }
 
-export type { EmploymentTypeInterface };
+interface CreateEmploymentTypeInterface {
+  employmentName: string;
+  workingHours: number;
+}
+
+interface EmploymentTypePropsInterface {
+  name: string;
+  type: string;
+  label: string;
+}
+
+const EmploymentTypeSchema = object({
+  employmentName: z.string().min(3, { message: "Name is too short" }),
+  workingHours: z.number().max(672, { message: "Working hours is too long" }),
+});
+
+export type {
+  EmploymentTypeInterface,
+  CreateEmploymentTypeInterface,
+  EmploymentTypePropsInterface,
+};
+
+export { EmploymentTypeSchema };
