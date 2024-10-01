7946d07025e93d551b12c64f81ff5fc58c93679a
podlaskialgorytm <live.m851@gmail.com>
2024-10-01 16:03:58 +0200

    feat: create function, which open a popup with question if user is sure to delete day restirction

--- src/features/schedule-rules/components/day-restriction-view.tsx
+++ src/features/schedule-rules/components/day-restriction-view.tsx
@@ -4,10 +4,25 @@ import { Loading } from "../../../shared/components/loading";
 
 import { DayRestrictionInterface } from "../types/types";
 
+import Swal from "sweetalert2";
+
 const DayRestrictionView: React.FC<{ scheduleRuleId: string }> = ({
   scheduleRuleId,
 }) => {
   const { data, isLoading } = useFetchDayRestriction({ scheduleRuleId });
+
+  const handleDelete = (dayRestrictionId: string) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+    });
+  };
 
   return (
     <div>
@@ -16,7 +31,10 @@ const DayRestrictionView: React.FC<{ scheduleRuleId: string }> = ({
         {isLoading && <Loading size={50} />}
         {data?.map(
           (dayRestriction: DayRestrictionInterface & { id: string }) => (
-            <div className="flex justify-between border-primary border-[2px] border-solid mb-4 rounded-lg p-3">
+            <div
+              className="flex justify-between border-primary border-[2px] border-solid mb-4 rounded-lg p-3"
+              onClick={() => handleDelete(dayRestriction.id as string)}
+            >
               <p>
                 <b>{dayRestriction.dayOfWeek}</b> for max{" "}
                 <b>{dayRestriction.maxFollowingDay}</b> time in a row
